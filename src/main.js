5c7fd5e4f6c5dac0f899c4a8d11a2d84db76c046
wangcd <wangchendong@innjoy.com>
2020-11-06 18:06:35 +0800

    '标题栏'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+import { Menu, MenuItem, Submenu } from 'element-ui';
 import './assets/css/global.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
 
 /* eslint-disable no-new */
 new Vue({
