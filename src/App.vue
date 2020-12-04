aa7f4acaf5b9f11b821b3adace8a5cc27724a221
wangcd <wangchendong@innjoy.com>
2020-12-04 16:41:36 +0800

    '添加备案号'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <router-view />
+    <div class="beian">
+      <a target="_blank" style="color: #5c6b77" href="http://www.beian.miit.gov.cn">陕ICP备2020017210号-1</a>&nbsp;
+    </div>
   </div>
 </template>
 
@@ -13,5 +16,13 @@ export default {
 <style>
 #app {
   height: 100%;
+}
+.beian {
+  position: fixed;
+  text-align: center;
+  bottom: 0px;
+  margin-bottom: 15px;
+  width: 100%;
+  color: #5c6b77;
 }
 </style>
